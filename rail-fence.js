00ecc7048e6ef49020b3dbeef42d2f97202f15ca
Rishi Dutt Shukla <duttrishi72@gmail.com>
2020-09-14 21:09:28 +0530

    Used a textarea instead of input type="text"

--- rail-fence.js
+++ rail-fence.js
@@ -1,6 +1,6 @@
 function encrypt() {
-    var inputString = document.getElementById("inputString").value
-    var n = document.getElementById("n").value
+    var inputString = document.getElementById("originalText").value
+    var key = document.getElementById("n").value
     var inputArray = inputString.split("")
     var indexPosition = 0;
     var processedArrayGrid  = new Array(10);
@@ -8,77 +8,86 @@ function encrypt() {
     var indexPosition = 0;
     var indexPositionIncreasing = true
 
-    //creating the encryption array
-    for(var i = 0; i < inputArray.length; i++) {
-        processedArrayGrid[i] = new Array(n.toNumber);
-        processedArrayGrid[i][indexPosition] = inputArray[i];
-        if(indexPositionIncreasing == true) {
-            indexPosition += 1;
-        } else {
-            indexPosition -= 1;
-        }
-        if(indexPosition == n - 1) {
-            indexPositionIncreasing = false;
-        } else if(indexPosition == 0) {
-            indexPositionIncreasing = true;
-        }
-        
-    }
-    console.table(processedArrayGrid)
-    //printing out the ciphertext
-    for(var j = 0; j < n; j++) {
+    if(key == null || key == 0) {
+        alert('key should not be zero/null')
+    } else {
+        //creating the encryption array
         for(var i = 0; i < inputArray.length; i++) {
-            outputString += processedArrayGrid[i][j] == undefined  ? "":processedArrayGrid[i][j];
+            processedArrayGrid[i] = new Array(n.toNumber);
+            processedArrayGrid[i][indexPosition] = inputArray[i];
+            if(indexPositionIncreasing == true) {
+                indexPosition += 1;
+            } else {
+                indexPosition -= 1;
+            }
+            if(indexPosition == key - 1) {
+                indexPositionIncreasing = false;
+            } else if(indexPosition == 0) {
+                indexPositionIncreasing = true;
+            }
+            
         }
-    }
+        console.table(processedArrayGrid)
+        //printing out the ciphertext
+        for(var j = 0; j < key; j++) {
+            for(var i = 0; i < inputArray.length; i++) {
+                outputString += processedArrayGrid[i][j] == undefined  ? "":processedArrayGrid[i][j];
+            }
+        }
 
-    document.getElementById("outputString").innerHTML = outputString;
+        document.getElementById("encryptedText").value = outputString;
+        }
 }
 
 function decrypt() {
     //getting the ciphertext
-    var encryptedString = document.getElementById("outputString").innerHTML;
-    const encryptedStringLength = encryptedString.length;
+    var cipherText = document.getElementById("encryptedText").value;
+    const cipherTextLength = cipherText.length;
     //getting key
     var key = document.getElementById("n").value;
     //splitting into array
-    const encryptedStringArray = encryptedString.split("");
+    const cipherTextArray = cipherText.split("");
     var decryptedString = "";
     //creating the array
-    var decryptedArray = new Array(encryptedStringArray.length)
+    var decryptedArray = new Array(cipherTextArray.length)
     var indexPosition = 0;
     var indexPositionIncreasing = true
 
-    //filling the decrypted array with placeholder
-    for(var i = 0; i < encryptedString.length; i++) {
-        decryptedArray[i] = new Array(key.toNumber);
-        decryptedArray[i][indexPosition] = '*';
-        if(indexPositionIncreasing == true) {
-            indexPosition += 1;
-        } else {
-            indexPosition -= 1;
-        }
-        if(indexPosition == key - 1) {
-            indexPositionIncreasing = false;
-        } else if(indexPosition == 0) {
-            indexPositionIncreasing = true;
-        }
-    }
-    
-    var index = 0
-    for(var j = 0; j < key; j++) {
-        for(var i = 0; i < encryptedStringLength; i++) {
-            if(decryptedArray[i][j] == '*' && index < encryptedStringLength) {
-                decryptedArray[i][j] = encryptedStringArray[index];
-                index++;
+    if(key == null || key == 0) {
+        alert('key should not be zero/null')
+    } else {
+        //filling the decrypted array with placeholders - '*'
+        for(var i = 0; i < cipherText.length; i++) {
+            decryptedArray[i] = new Array(key.toNumber);
+            decryptedArray[i][indexPosition] = '*';
+            if(indexPositionIncreasing == true) {
+                indexPosition += 1;
+            } else {
+                indexPosition -= 1;
+            }
+            if(indexPosition == key - 1) {
+                indexPositionIncreasing = false;
+            } else if(indexPosition == 0) {
+                indexPositionIncreasing = true;
             }
         }
-    }
-    for(var i = 0; i < encryptedStringLength; i++) {
+        //creating the array
+        var index = 0
         for(var j = 0; j < key; j++) {
-            decryptedString += decryptedArray[i][j] == undefined ? "":decryptedArray[i][j];
+            for(var i = 0; i < cipherTextLength; i++) {
+                if(decryptedArray[i][j] == '*' && index < cipherTextLength) {
+                    decryptedArray[i][j] = cipherTextArray[index];
+                    index++;
+                }
+            }
         }
+        //printing out
+        for(var i = 0; i < cipherTextLength; i++) {
+            for(var j = 0; j < key; j++) {
+                decryptedString += decryptedArray[i][j] == undefined ? "":decryptedArray[i][j];
+            }
+        }
+        console.table(decryptedArray)
+        document.getElementById("originalText").value = decryptedString;
     }
-    console.table(decryptedArray)
-    document.getElementById("decryptedString").innerHTML = decryptedString;
 }
